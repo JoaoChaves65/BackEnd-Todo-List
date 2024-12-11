dd86f5edc994a54505e3eb457024a3653e39b40b
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2024-12-11 09:49:43 -0300

    Adicionando arquivos de configuração

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import taskRoutes from "./routes/taskRoutes";
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
+
+// Rotas
+app.use("/tasks", taskRoutes);
 
 export default app;
